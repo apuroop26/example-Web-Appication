744e356f5dfde6ceb1bf4925573bf374e59505e3
apuroop <apuroop.paleti@gmail.com>
2016-08-30 16:15:06 +0200

    still need to figure out some issues

--- backend/src/main/java/se/example2/softhouse/DemoConfiguration.java
+++ backend/src/main/java/se/example2/softhouse/DemoConfiguration.java
@@ -1,31 +1,64 @@
 package se.example2.softhouse;
 
-import com.bazaarvoice.dropwizard.assets.AssetsBundleConfiguration;
-import com.bazaarvoice.dropwizard.assets.AssetsConfiguration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
-import se.example2.softhouse.Note.Note;
-
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.db.PooledDataSourceFactory;
+import org.hibernate.validator.constraints.NotEmpty;
+import com.sun.istack.internal.NotNull;
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 /**
  * Created by hxs on 2016-08-16.
  */
-public class DemoConfiguration extends Configuration implements AssetsBundleConfiguration{
+public class DemoConfiguration extends Configuration {
 
-    private Note defaultNote;
+    @NotEmpty
+    private String template;
 
-    public Note getDefaultNote() {
-        return defaultNote;
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    private String[] allowedOrigins;
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
     }
 
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+
+    @JsonProperty
+    public String[] getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    @JsonProperty
+    public void setAllowedOrigins(String[] allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+    @JsonProperty
     @Valid
     @NotNull
-    @JsonProperty
-    private final AssetsConfiguration assets = new AssetsConfiguration();
+    private DataSourceFactory database = new DataSourceFactory();
 
-    public AssetsConfiguration getAssetsConfiguration() {
-        return assets;
+    public PooledDataSourceFactory getDatabaseConfiguration() {
+    return database;
     }
+
+
 }
